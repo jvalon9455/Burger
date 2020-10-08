68cf4c77c4fc6adae6c1151968184c121e46f561
John Alonso <jvalon9455@gmail.com>
2020-10-08 15:17:56 -0400

    added middleware and routes

--- server.js
+++ server.js
@@ -2,3 +2,25 @@ const express = require("express");
 const mysql = require("mysql");
 const exphbs = require("express-handlebars");
 
+const app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res)=> {
+    res.render("index");
+})
+
+app.get("/api/config", (req, res) => {
+    res.json({
+        success: true,
+    })
+});
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
