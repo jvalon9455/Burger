df4b5d72f00087c6e7e492f430b10497772eb96d
John Alonso <jvalon9455@gmail.com>
2020-10-08 14:30:04 -0400

    added db in sql and workbench

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
 -- In this file, write insert queries to populate the `burgers` table with at least three entries.
 
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Cheeseburger');
