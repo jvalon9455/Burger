e44a8930d6e732188b20fc34cd7300f14dff2e76
John Alonso <jvalon9455@gmail.com>
2020-10-08 21:03:52 -0400

    added link to burger.js and fixed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 -- In this file, write insert queries to populate the `burgers` table with at least three entries.
 
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Cheeseburger', false);
