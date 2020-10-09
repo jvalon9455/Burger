f5c381827aa88baab90fbf604c1c9be4c8291512
John Alonso <jvalon9455@gmail.com>
2020-10-08 21:13:38 -0400

    fixed issue in connection.js and added to get route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const exphbs = require("express-handlebars");
+const connection = require("./config/connection");
 
 const app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,7 +12,10 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
+    connection.query("SELECT * FROM burger", (err, data) => {
+        console.table(data);
+    })
     res.render("index");
 })
 
@@ -24,4 +28,3 @@ app.get("/api/config", (req, res) => {
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
-  
