7b47c54e9005efe687e1a108d9cf429ba6f73d6b
John Alonso <jvalon9455@gmail.com>
2020-10-08 22:49:39 -0400

    copied over orm and added routes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,7 +2,25 @@ var express = require("express");
 var burger = require("../models/burger");
 var router = express.Router();
 
+router.get("/", function (req, res) {
+    cat.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-
+router.post("/api/cats", function (req, res) {
+    burgers.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
 
 module.exports = router;
