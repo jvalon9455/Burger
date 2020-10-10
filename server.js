9fac13b059fcfd354b4339cc7dc5ec8a68ced771
John Alonso <jvalon9455@gmail.com>
2020-10-09 22:48:14 -0400

    started over because previous code was not working with me

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
-const express = require("express");
-// const mysql = require("mysql");
-const exphbs = require("express-handlebars");
-const connection = require("./config/connection");
+var express = require("express");
 
-const app = express();
 var PORT = process.env.PORT || 3000;
 
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-
-// middleware for handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-    connection.query("SELECT * FROM burgers", (err, data) => {
-        console.table(data);
-    })
-    res.render("index");
-})
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controllers.js");
 
-app.get("/api/config", (req, res) => {
-    res.json({
-        success: true,
-    })
-});
-
-// get routes to access server
-const routes = require ("./controllers/burgers_controllers")
 app.use(routes);
 
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
